6304044d60fce3c8b8696637de7e0f545376f99d
Toymaker0 <vasanthtoy@gmail.com>
2025-05-17 09:29:13 +0530

    setup

--- billing_client/src/Component/login/Login.js
+++ billing_client/src/Component/login/Login.js
@@ -8,79 +8,79 @@ import { setUserId } from '../../Redux/Slice';
 
 
 const Login = () => {
-  const navigate=useNavigate()  
-  const [loginUser,setLoginUser]=useState({})
-  const dispatch=useDispatch()
+  const navigate = useNavigate()
+  const [loginUser, setLoginUser] = useState({})
+  const dispatch = useDispatch()
 
 
-    const loged = () => toast("succesfully login 😍");
-    const incorrect = () => toast("Incorrect Password 🫡");
-    const noUser = () => toast("User does not exist");
-    const userisblocked=()=>toast('you are blocked contact Admin')
+  const loged = () => toast("succesfully login 😍");
+  const incorrect = () => toast("Incorrect Password 🫡");
+  const noUser = () => toast("User does not exist");
+  const userisblocked = () => toast('you are blocked contact Admin')
 
-    const handleChangeLogin=(e)=>{
-        setLoginUser({...loginUser,[e.target.name]:e.target.value})
-    }
-    const logIn=(e)=>{
-      loged()
-      navigate('/selectionArea')
-    }
-    const handleLogin=async(e)=>{
-      e.preventDefault()
-       const login=await axios.post("/auth/login",loginUser)
-       console.log(login);
-       
-       if(login){
-        if(login.data.message=='Authurized'){
-      if(login.data.result[0].isblock==0){
-        const userId=login.data.result[0].id
-        sessionStorage.setItem('userId',JSON.stringify(userId))  // session storage
+  const handleChangeLogin = (e) => {
+    setLoginUser({ ...loginUser, [e.target.name]: e.target.value })
+  }
+  const logIn = (e) => {
+    loged()
+    navigate('/selectionArea')
+  }
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    const login = await axios.post("/auth/login", loginUser)
+    console.log(login);
 
-        dispatch(setUserId(userId)) //Redux storage
-        logIn()
+    if (login.data) {
+
+        if (login.data.result[0].isblock == 0) {
+          const userId = login.data.result[0].id
+          sessionStorage.setItem('userId', JSON.stringify(userId))  // session storage
+
+          dispatch(setUserId(userId)) //Redux storage
+          logIn()
+        }
+        else {
+          userisblocked()
+        }
+
       }
-      else{
-        userisblocked()
+      else if (login.data.message == 'incorrect passsword') {
+        console.log(2);
+        incorrect()
       }
-          
-        }
-        else if(login.data.message=='incorrect passsword'){
-          console.log(2);
-          incorrect()
-        }
-        else{
-          noUser()
-        }
-       }
+      else {
+        noUser()
+      }
     }
-   
+  
+
   return (
     <div className='login'>
       <form action="" id='formId'>
-      <div>
-        <ToastContainer />
-      </div>
-      <div className="login_content_box">
-        <h1>LOGIN</h1>
-        <div className="login_input_box">
-          <input type="text" name='userName' id='uName' onChange={handleChangeLogin}  required/>
-          <label htmlFor="uName">User Name</label>
+        <div>
+          <ToastContainer />
         </div>
+        <div className="login_content_box">
+          <h1>LOGIN</h1>
+          <div className="login_input_box">
+            <input type="text" name='userName' id='uName' onChange={handleChangeLogin} required />
+            <label htmlFor="uName">User Name</label>
+          </div>
 
-        <div className="login_input_box">
-          <input type="password" name='password' id='uPassword' onChange={handleChangeLogin} required/>
-          <label htmlFor="uPassword">Password</label>
-        </div>
+          <div className="login_input_box">
+            <input type="password" name='password' id='uPassword' onChange={handleChangeLogin} required />
+            <label htmlFor="uPassword">Password</label>
+          </div>
 
-        <div className='submin_box'>
-          <button className='btn btn-secondary ' onClick={(e)=>{handleLogin(e)}}  >Log In</button>
+          <div className='submin_box'>
+            <button className='btn btn-secondary ' onClick={(e) => { handleLogin(e) }}  >Log In</button>
+          </div>
+          <div className="signUp_login">
+            <div className='toogle_section'> Not have a account <span className='signUp loginSignUp_Link'> Contact Admin</span></div>
+          </div>
         </div>
-        <div className="signUp_login">
-          <div className='toogle_section'> Not have a account <span className='signUp loginSignUp_Link'> Contact Admin</span></div>
-        </div>
-      </div>
       </form>
-   
+
     </div>
   )
 }
--- server/src/module/auth/auth.service.ts
+++ server/src/module/auth/auth.service.ts
@@ -4,60 +4,57 @@ import { DbProvider } from 'src/Database/db';
 @Injectable()
 export class AuthService {
 
-    constructor(private readonly db: DbProvider){  }
+  constructor(private readonly db: DbProvider) { }
 
-    async login(payLoad: any) {
-        console.log(payLoad.userName, payLoad.password);
-        let x = await this.db
-            .getDb()
-            .table("Users")
-            .select(['userName', 'password'])
-            .whereLike("userName", `%${payLoad.userName}%`)
-            .first();
-            console.log(x);
-            
-          const auth= await this.comparePasswords(payLoad.password,x.password)
-          if(auth){
-            return true
-          }
+  async login(payLoad: any) {
+    let query = await this.db
+      .getDb()
+      .table("Users")
+      .select(['userName', 'password', 'firstName', 'lastName', 'roleId', 'isBlock', 'createdAt', 'updatedAt'])
+      .whereLike("userName", `%${payLoad.userName}%`)
+      .first();
+
+    const auth = await this.comparePasswords(payLoad.password, query.password)
+    if (auth) {
+      delete query.password
+      return query
+    }
+  }
+
+  async comparePasswords(plainPassword: string, hashedPassword: string): Promise<boolean> {
+    return bcrypt.compare(plainPassword, hashedPassword); // Returns true/false
+  }
+
+  async handlePassword(payLoad: any) {
+    try {
+      if (payLoad?.password == payLoad?.confirmPass) {
+        const password = payLoad.password
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
+        payLoad.password = hash
+        delete payLoad.confirmPass
+        return payLoad
+        //    async function comparePasswords(plainPassword: string, hashedPassword: string): Promise<boolean> {
+        //     return bcrypt.compare(plainPassword, hashedPassword); // Returns true/false
+        //   }
+      }
+    } catch (error) {
+
     }
 
-    async  comparePasswords(plainPassword: string, hashedPassword: string): Promise<boolean> {
-        return bcrypt.compare(plainPassword, hashedPassword); // Returns true/false
-      }
+  }
 
-      async handlePassword(payLoad:any){
-        try {
-           if (payLoad?.password == payLoad?.confirmPass) {
-            const password = payLoad.password
-            const salt = await bcrypt.genSalt(10)
-            const hash = await bcrypt.hash(password, salt)
-            payLoad.password = hash
-            delete payLoad.confirmPass
-            return payLoad
-            //    async function comparePasswords(plainPassword: string, hashedPassword: string): Promise<boolean> {
-            //     return bcrypt.compare(plainPassword, hashedPassword); // Returns true/false
-            //   }
-        }
-        } catch (error) {
-          
-        }
+  async createAuth(payLoad: any) {
+    try {
+      const query = await this.db
+        .getDb()
+        .table("Users")
+        .insert(payLoad)
+        .returning('id')
+      return await query
+    } catch (error) {
+      console.log(error);
 
-      }
-
-      async createAuth(payLoad:any){
-        try {
-        const query = await this.db
-            .getDb()
-            .table("Users")
-            .insert(payLoad)
-            .returning('id')
-           
-           
-         return await query
-        } catch (error) {
-          console.log(error);
-          
-        }
-      }
+    }
+  }
 }
